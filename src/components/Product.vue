ecfab1e1e3779c3c70c18a26a033aed585ed7442
sefaemrahoglu <sefaemrahoglu@hotmail.com>
2020-05-15 18:05:00 +0300

    final

---
+++ src/components/Product.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="col-md-2 card">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Progress.vue
@@ -0,0 +1,31 @@
+<template>
+      <div class="progress">
+          <div 
+          class="progress-bar bg-info" role="progressbar" 
+          :style="{width: progress + '%'}" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">{{ currentItem }}/10
+          </div>
+      </div>
+</template>
+
+<script>
+import { eventBus } from '../main';
+export default {
+    data(){
+        return {
+            currentItem : null,
+            progress : null
+        }
+    },
+    created(){
+        eventBus.$on("progressBarUpdated", (productListCount) => {
+            this.currentItem = productListCount;
+            this.progress = productListCount * 10;
+        });
+    }
+
+}
+</script>
+
+<style>
+
+</style>
